e009e4c0ff58d6f04a247671f51a85a3ac6bcdc4
chinesealex <liz75@mcmaster.ca>
2016-08-17 22:31:30 +0800

    Add number spinner for each dish

--- scripts/main.js
+++ scripts/main.js
@@ -8,7 +8,47 @@ var DishCategoryRow = React.createClass({
     }
 });
 
+var NumberSpinner = React.createClass({
+    render: function() {
+        return (
+            <div className="input-group number-spinner">
+                <span className="input-group-btn data-dwn">
+                    <button className="btn btn-default btn-info" onClick={this.props.onMinus} disabled={this.props.disabled}>
+                        <span className="glyphicon glyphicon-minus"></span>
+                    </button>
+                </span>
+                <input type="text" className="form-control text-center" value={this.props.dishCopyNumber} readOnly/>
+                <span className="input-group-btn data-up">
+                    <button className="btn btn-default btn-info" onClick={this.props.onPlus} disabled={this.props.disabled}>
+                        <span className="glyphicon glyphicon-plus"></span>
+                    </button>
+                </span>
+            </div>
+        );
+    }
+})
+
 var DishRow = React.createClass({
+    getInitialState: function() {
+        return {
+            dishCopyNumber: this.props.dish.copies
+        }
+    },
+
+    handleMinusCopy: function() {
+        this.setState({
+            dishCopyNumber: (this.state.dishCopyNumber > 1) ?
+                            this.state.dishCopyNumber - 1:
+                            this.state.dishCopyNumber
+        })
+    },
+
+    handlePlusCopy: function() {
+        this.setState({
+            dishCopyNumber: this.state.dishCopyNumber + 1,
+        })
+    },
+
     handleDelete: function() {
         this.props.onOrderDelete(this.props.dish);
     },
@@ -27,15 +67,32 @@ var DishRow = React.createClass({
             "$"+this.props.dish.price;
         
         var deleteBtn = this.props.ordered ?
-            <input type="button" value="Ordered" disabled={true}/> :
-            <input type="button" value="Delete" onClick={this.handleDelete} disabled={false}/>
+            <input type="button" className="btn btn-default btn-info" value="Ordered" disabled={true}/> :
+            <input type="button" className="btn btn-default btn-info" value="Delete" onClick={this.handleDelete} disabled={false}/>
+
+        var numberSpinner = this.props.ordered ?
+            <NumberSpinner 
+                dishCopyNumber={this.state.dishCopyNumber} 
+                onMinus={this.handleMinusCopy}
+                onPlus={this.handlePlusCopy} 
+                disabled={true}
+            /> :
+            <NumberSpinner 
+                dishCopyNumber={this.state.dishCopyNumber} 
+                onMinus={this.handleMinusCopy}
+                onPlus={this.handlePlusCopy}
+                disabled={false} 
+            />
 
         return (
             <div className="col-xs-12 jumbotron">
                 <img src={this.props.dish.imageURL} style={{"width":"100%", "height":"100%"}} />
                 <p className="text-center">{name}</p>
                 <p className="text-center">{price}</p>
-                <div>{deleteBtn}</div>
+                <div className="row">
+                    <div className="col-xs-6">{deleteBtn}</div>
+                    <div className="col-xs-6">{numberSpinner}</div>
+                </div>
             </div>
         );
     }
@@ -103,7 +160,7 @@ var SubtotalAndOrder = React.createClass({
                     value="Order Now"
                     ref="submitOrderInput" 
                     onClick={this.handleClick}
-                    className="btn-primary"
+                    className="btn btn-default btn-info"
                 />
             </div>
         );
@@ -153,12 +210,12 @@ var DeletableDishTable = React.createClass({
 });
 
 var DISHES = [
-    {category: 'Chicken', price: '10.99', imageURL: '/img/gongbao_chicken.jpg', name: 'Gongbao Chicken'},
-    {category: 'Chicken', price: '9.99', imageURL: '/img/fried_chicken_wing.jpg', name: 'Chicken Wing'},
-    {category: 'Chicken', price: '12.99', imageURL: '/img/mcnuggets.jpg', name: 'McNugget'},
-    {category: 'Beef', price: '8.99', imageURL: '/img/beef_stew.jpg', name: 'Beef Stew'},
-    {category: 'Beef', price: '6.99', imageURL: '/img/beef_burger.jpg', name: 'Beef Burger'},
-    {category: 'Lamb', price: '7.99', imageURL: '/img/whole_lamb.jpg', name: 'BBQ Whole Lamb'}
+    {category: 'Chicken', price: '10.99', imageURL: '/img/gongbao_chicken.jpg', name: 'Gongbao Chicken', copies: 2},
+    {category: 'Chicken', price: '9.99', imageURL: '/img/fried_chicken_wing.jpg', name: 'Chicken Wing', copies: 3},
+    {category: 'Chicken', price: '12.99', imageURL: '/img/mcnuggets.jpg', name: 'McNugget', copies: 1},
+    {category: 'Beef', price: '8.99', imageURL: '/img/beef_stew.jpg', name: 'Beef Stew', copies: 3},
+    {category: 'Beef', price: '6.99', imageURL: '/img/beef_burger.jpg', name: 'Beef Burger', copies: 2},
+    {category: 'Lamb', price: '7.99', imageURL: '/img/whole_lamb.jpg', name: 'BBQ Whole Lamb', copies: 4}
 ];
 
 
