efb5591dc67c3ec957ee077b29509e6f855dbb4c
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-06 16:34:42 -0400

    feature/CORE-04092017-project-setup
    * Export firebase configuration

--- src/app/firebaseConfig.js
+++ src/app/firebaseConfig.js
@@ -1,11 +1,9 @@
-import * as firebase from "firebase"
-
-// Initialize Firebase
-var DefaultConfig = {
+// Firebase configuration for Instaurant
+const firebaseConfig = {
     apiKey: "AIzaSyBpRpP_p6-9wagoZRPbJpqoGkJD5028JFk",
     authDomain: "instaurant-72a63.firebaseapp.com",
     databaseURL: "https://instaurant-72a63.firebaseio.com",
     storageBucket: "instaurant-72a63.appspot.com",
 };
-// firebase is a global namespace from which all the Firebase services are accessed.
-firebase.initializeApp(DefaultConfig);
+
+export default firebaseConfig;
