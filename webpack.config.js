670beda807020b259910001a6db04a0ce6cf4a19
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-10 20:46:51 -0400

    feature/CORE-04092017-project-setup
    * Add webpack-dev-server and react hot loader dependencies
    * Enable webpack hot module replacement and react hot loading
    * Change the output.publicPath to include a forward slash

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,26 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
-//const webpack = require('webpack'); //to access built-in plugins
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // Installed via npm
+const webpack = require('webpack'); // To access built-in plugins
 
 const config = {
     entry: {
-        main: './src/app/index.js'
+        main: [
+            'webpack-dev-server/client?http://localhost:8080', // Enable websocket connection (needs url and port)
+            'webpack/hot/only-dev-server', // Perform HMR in the browser BUT doesn't reload the page upon errors
+            //'webpack/hot/dev-server', // Perform HMR in the browser and reload the page upon errors
+            './src/app/index.js'
+        ]
     },
     output: {
         filename:'[name].js',
-        publicPath: 'bundles/', // The publicPath also affects the relative path of the injected scripts to index.html
+        publicPath: '/bundles/', // The publicPath also affects the relative path of the injected scripts to index.html
         path: path.resolve('bundles')
     },
     module: {
         // Rules for the module (configure loaders, parser option, etc.)
         rules: [
             // No qoute around the tested egRex
-            {test: /\.jsx?$/, loader: "babel-loader", include: path.resolve('./src/app')}
+            {test: /\.jsx?$/, loader: "react-hot-loader!babel-loader", include: path.resolve('./src/app')}
         ]
     },
     resolve: {
@@ -31,8 +36,13 @@ const config = {
             template: './src/index.template.html',
             filename: '../index.html',
             inject: 'body'
-        })
-    ]
+        }),
+        new webpack.HotModuleReplacementPlugin() // To generate hot update chunks
+    ],
+    devServer: {
+        hot: true, // Enable HMR in webpack-dev-server and in libs running in the the browser
+        contentBase: './' // Tell webpack dev server where to serve content (static assets) from
+    }
 }
 
 module.exports = config;
