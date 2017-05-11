8e08676933f61d0acabdbe5e237fc1cd54282894
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-10 21:09:56 -0400

    feature/CORE-04092017-project-setup
    * Call module.hot.accept() explicitly to make hot loading work (otherwise keep refreshing)
    * minor change to HomeIndex.jsx

--- src/app/components/IndexHome.jsx
+++ src/app/components/IndexHome.jsx
@@ -1,3 +1,5 @@
 import React from 'react';
 
-export default () => (<h1>This is the Home Page of the application!</h1>);
+export default (props) => (
+    <h1>This is the Home Page of the application!</h1>
+);
--- src/app/index.js
+++ src/app/index.js
@@ -4,7 +4,9 @@ import ReactDom from 'react-dom';
 import HomePage from './components/IndexHome';
 import FirebaseApp from './firebaseService';
 
-ReactDom.render(<HomePage/>, document.getElementById('root'));
+// Hacking to avoid refreshing page, we want hot reloading!
+if (module.hot) {
+    module.hot.accept();
+}
 
-console.log("The auth object is: ");
-console.log(FirebaseApp.auth());
+ReactDom.render(<HomePage/>, document.getElementById('root'));
