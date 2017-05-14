e401b48d5f27bc61689075e961c68f1c92ced97b
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-14 16:37:15 -0400

    feature/CORE-05102017-owner-login-ui
    * Add a service method to check whether there is a signed-in user
    * Add lifecycle method to AdministrationPanel component to redirect user to the sign in page if no sign in

--- src/app/components/AdministrationPanel.jsx
+++ src/app/components/AdministrationPanel.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import isUserSignedIn from '../firebaseService';
 
-const AdministrationPanel = () => (
-    <h1>This is the Administration Panel page!</h1>
-);
+class AdministrationPanel extends Component {
+
+    // If there is no signed-in user, redirect to the sign in page
+    componentWillMount() {
+        const { history } = this.props;
+        if (!isUserSignedIn) {
+            history.replace('/signin');
+        }
+    }
+
+    render() {
+        return <h1>This is the Administration Panel page!</h1>;
+    }
+}
+
+AdministrationPanel.propTypes = {
+    history: PropTypes.object.isRequired,
+};
 
 export default AdministrationPanel;
--- src/app/firebaseService.js
+++ src/app/firebaseService.js
@@ -4,6 +4,15 @@
 import * as firebase from 'firebase';
 import firebaseConfig from './firebaseConfig';
 
+// Webpack hotloading will cause firebase app reinitialization
 const firebaseApp = firebase.initializeApp(firebaseConfig);
+const firebaseAuth = firebaseApp.auth();
+// const firebaseDatabase = firebaseApp.database();
 
-export default firebaseApp;
+/**
+ * Check whether there is a signed-in user
+ * @return true if user is signed-in
+ */
+export const isUserSignedIn = () => firebaseAuth.currentUser != null;
+
+);
