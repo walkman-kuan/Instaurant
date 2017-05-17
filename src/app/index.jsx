aa4fa467d7a2e43a6609999dd1b81dbd106fca70
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-16 23:43:07 -0400

    feature/CORE-05102017-owner-login-ui
    * Configure less-loader to compile less into css
    * Configure css-loader to interprets @import and url() like import/require() and resolve them
    * Configure style-loader to adds CSS to the DOM by injecting a <style> tag (after the whole js bundle is loaded)
    * Configure ExtractTextWebpackPlugin to extract the css bundle into s separate file
    * Create the custom less file
    * Put the extracted css file into the template index.html
    * Comment out ExtractTextWebpackPlugin functionality in development since it doesn’t support HMR

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -3,6 +3,8 @@ import ReactDom from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import InstaurantOwner from './components/InstaurantOwner';
 
+import './less/custom.less';
+
 // Hacking to avoid refreshing page, we want hot reloading!
 if (module.hot) {
     module.hot.accept();
