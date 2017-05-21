34cc5d0723c215829a16748ee9da4fdc4bee8b8b
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-20 22:40:13 -0400

    feature/CORE-05202017-integrate-firebase-auth-sign-up
    * Rename firebaseConfig.js to firebaseApp.js
    * Move firebase app initialization to firebaseApp.js to avoid repeated initializations due to webpack hot loading

--- src/app/firebaseApp.js
+++ src/app/firebaseApp.js
@@ -1,3 +1,5 @@
+import * as firebase from 'firebase';
+
 // Firebase configuration for Instaurant
 const firebaseConfig = {
     apiKey: 'AIzaSyBpRpP_p6-9wagoZRPbJpqoGkJD5028JFk',
@@ -6,4 +8,4 @@ const firebaseConfig = {
     storageBucket: 'instaurant-72a63.appspot.com',
 };
 
-export default firebaseConfig;
+export default firebase.initializeApp(firebaseConfig);
--- src/app/firebaseService.js
+++ src/app/firebaseService.js
@@ -1,11 +1,7 @@
 // The firebase specific requests are defined, and will be exported to the async action
 // creator file.
+import firebaseApp from './firebaseApp';
 
-import * as firebase from 'firebase';
-import firebaseConfig from './firebaseConfig';
-
-// Webpack hotloading will cause firebase app reinitialization
-const firebaseApp = firebase.initializeApp(firebaseConfig);
 const firebaseAuth = firebaseApp.auth();
 // const firebaseDatabase = firebaseApp.database();
 
