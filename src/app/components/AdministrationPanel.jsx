94198bbc7071e01bdddbafcc71b05184ac82e874
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-22 14:27:05 -0400

    feature/CORE-05212017-create-admin-layout
    * Create the Navigation Bar (Brand, Profile and Sign Out) for the Admin page

--- src/app/components/AdministrationPanel.jsx
+++ src/app/components/AdministrationPanel.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { isUserSignedIn } from '../firebaseService';
+import { Link } from 'react-router-dom';
+import { isUserSignedIn, signOut } from '../firebaseService';
 
 class AdministrationPanel extends Component {
+    constructor() {
+        super();
+        this.onSignOut = this.onSignOut.bind(this);
+    }
 
     // If there is no signed-in user, redirect to the sign in page
     componentWillMount() {
@@ -12,13 +17,54 @@ class AdministrationPanel extends Component {
         }
     }
 
+    onSignOut() {
+        signOut();
+    }
+
     render() {
-        return <h1>This is the Administration Panel page!</h1>;
+        return (
+            <div className="admin-page-wrapper">
+                <nav className="navbar navbar-default">
+                    <div className="container-fluid">
+                        <div className="navbar-header">
+                            <button
+                              type="button"
+                              className="navbar-toggle"
+                              data-toggle="collapse"
+                              data-target="#owner-nav-bar"
+                            >
+                                <span className="icon-bar" />
+                                <span className="icon-bar" />
+                                <span className="icon-bar" />
+                            </button>
+                            {/* Stay on current page if click */}
+                            <Link to={this.props.location} className="navbar-brand">Instaurant</Link>
+                        </div>
+                        <div className="collapse navbar-collapse" id="owner-nav-bar">
+                            <ul className="nav navbar-nav navbar-right">
+                                <li>
+                                    <Link to="/profile">
+                                        <span className="glyphicon glyphicon-user" />&nbsp;Profile
+                                    </Link>
+                                </li>
+                                <li>
+                                    <Link to="/signin" onClick={this.onSignOut}>
+                                        <span className="glyphicon glyphicon-off" />&nbsp;Sign out
+                                    </Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                <h1>This is the Administration Panel page!</h1>
+            </div>
+        );
     }
 }
 
 AdministrationPanel.propTypes = {
     history: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired,
 };
 
 export default AdministrationPanel;
