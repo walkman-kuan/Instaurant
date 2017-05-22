2d72e4e754bb33a88ed5433f70a255e337662cb2
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-22 14:20:56 -0400

    feature/CORE-05212017-create-admin-layout
    * Add a fake profile page, and specify routing for it
    * Add some comment about routing and mount/unmount component

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -6,13 +6,16 @@ import SignIn from './components/SignIn';
 import SignUp from './components/SignUp';
 import ResetPassword from './components/ResetPassword';
 import AdministrationPanel from './components/AdministrationPanel';
+import Profile from './components/Profile';
 
 export default (
     <Switch>
+        {/* Routing to another component will UNMOUNT the current components! */}
         <Route exact path="/" component={IndexHome} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/reset-password" component={ResetPassword} />
         <Route path="/admin" component={AdministrationPanel} />
+        <Route path="/profile" component={Profile} />
     </Switch>
 );
