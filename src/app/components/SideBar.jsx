001668903e0a1a7986722bf75c2c49f45dcd26f6
Kuan Lin <linkuan_ant@hotmail.com>
2017-05-27 20:07:14 -0400

    feature/CORE-05212017-create-admin-layout
    * Create a sidebar header for category
    * Move the categories into a content wrapper in order to create a sticky footer
    * Create a sticky footer used to trigger category editing

--- src/app/components/SideBar.jsx
+++ src/app/components/SideBar.jsx
@@ -3,12 +3,17 @@ import PropTypes from 'prop-types';
 
 const SideBar = props => (
     <div id="sidebar" className="sidebar-wrapper" style={props.sidebarStyle}>
-        <div className="sidebar-header">
-            CATEGORY
+        <div className="sidebar-content">
+            <div className="sidebar-header">
+                Category
+            </div>
+            <ul className="sidebar-nav list-unstyled">
+                {props.categories}
+            </ul>
         </div>
-        <ul className="sidebar-nav list-unstyled">
-            {props.categories}
-        </ul>
+        <div className="sidebar-footer">
+            <span className="glyphicon glyphicon-edit" />&nbsp;Edit categories
+        </div>
     </div>
 );
 
