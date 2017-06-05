ed4147d289d742f14d39eed35e470c5d79c28bb3
Kuan Lin <linkuan_ant@hotmail.com>
2017-06-04 23:28:33 -0400

    feature/CORE-05212017-create-admin-layout
    * Convert NavBar to a functional component (may convert it to class component later if necessary)

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -1,60 +1,54 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import { signOut } from '../firebaseService';
 
-class NavBar extends Component {
-    constructor() {
-        super();
-        this.handleSignOut = this.handleSignOut.bind(this);
-    }
-
-    handleSignOut() {
+const NavBar = ({ onToggleSiderbar }) => {
+    const handleSignOut = () => {
+        console.log('Yo!');
         signOut();
-    }
+    };
 
-    render() {
-        return (
-            <nav className="navbar navbar-default">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button
-                          type="button"
-                          className="navbar-toggle sidebar-toggle"
-                          onClick={this.props.onToggleSiderbar}
-                        >
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                            <span className="icon-bar" />
-                        </button>
-                        {/* Put the logo here */}
-                        <Link to="/admin" className="navbar-brand">Instaurant</Link>
-                    </div>
-                    <ul className="nav navbar-nav right-aligned-nav">
-                        <li>
-                            <Link to="/qrCode">
-                                <span className="glyphicon glyphicon-qrcode" />
-                                <span className="nav-text">&nbsp;QR Code</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/profile">
-                                <span className="glyphicon glyphicon-user" />
-                                <span className="nav-text">&nbsp;Profile</span>
-                            </Link>
-                        </li>
-                        <li>
-                            <Link to="/signin" onClick={this.handleSignOut}>
-                                <span className="glyphicon glyphicon-off" />
-                                <span className="nav-text">&nbsp;Sign out</span>
-                            </Link>
-                        </li>
-                    </ul>
+    return (
+        <nav className="navbar navbar-default">
+            <div className="container-fluid">
+                <div className="navbar-header">
+                    <button
+                      type="button"
+                      className="navbar-toggle sidebar-toggle"
+                      onClick={onToggleSiderbar}
+                    >
+                        <span className="icon-bar" />
+                        <span className="icon-bar" />
+                        <span className="icon-bar" />
+                    </button>
+                    {/* Put the logo here */}
+                    <Link to="/admin" className="navbar-brand">Instaurant</Link>
                 </div>
-            </nav>
-        );
-    }
-}
+                <ul className="nav navbar-nav right-aligned-nav">
+                    <li>
+                        <Link to="/qrCode">
+                            <span className="glyphicon glyphicon-qrcode" />
+                            <span className="nav-text">&nbsp;QR Code</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/profile">
+                            <span className="glyphicon glyphicon-user" />
+                            <span className="nav-text">&nbsp;Profile</span>
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/signin" onClick={handleSignOut}>
+                            <span className="glyphicon glyphicon-off" />
+                            <span className="nav-text">&nbsp;Sign out</span>
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
 
 NavBar.propTypes = {
     onToggleSiderbar: PropTypes.func.isRequired,
