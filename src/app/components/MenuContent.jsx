42ee0e360a224c9ea7c55fcd9b2a26b1c952eb71
Kuan Lin <linkuan_ant@hotmail.com>
2017-06-27 20:34:12 -0400

    feature/CORE-06252017-dish-editing-with-redux-and-firebase
    * Add the AddDishThumbnail and AddDishModal components to the MenuContent component

--- src/app/components/MenuContent.jsx
+++ src/app/components/MenuContent.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
+import AddDishThumbnail from './AddDishThumbnail';
+import AddDishModal from './modals/AddDishModal';
 
 const MenuContent = () => (
     <div id="menu-content" className="menu-content">
         <div className="row">
-            <div className="col-xs-6 col-md-3">
+            <div className="col-sm-6 col-md-4 col-lg-3">
                 Image 1
             </div>
-            <div className="col-xs-6 col-md-3">
+            <div className="col-sm-6 col-md-4 col-lg-3">
                 Image 2
             </div>
-            <div className="col-xs-6 col-md-3">
+            <div className="col-sm-6 col-md-4 col-lg-3">
                 Image 3
             </div>
-            <div className="col-xs-6 col-md-3">
+            <div className="col-sm-6 col-md-4 col-lg-3">
                 Image 4
             </div>
+            <AddDishThumbnail />
         </div>
+        <AddDishModal />
     </div>
 );
 
