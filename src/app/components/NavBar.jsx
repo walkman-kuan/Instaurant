c141e667cfe277ca146fe9471d248e1807d7ffaf
Kuan Lin <linkuan_ant@hotmail.com>
2017-07-05 22:10:33 -0400

    feature/CORE-06252017-dish-editing-with-redux-and-firebase
    * Make the navbar fixed at the top
    * Prevent the fix navbar from overlaying sidebar and menu content by adjusting top or margin for sidebar and menu content

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -9,7 +9,7 @@ const NavBar = ({ onToggleSiderbar }) => {
     };
 
     return (
-        <nav className="navbar navbar-default">
+        <nav className="navbar navbar-default navbar-fixed-top">
             <div className="container-fluid">
                 <div className="navbar-header">
                     <button
